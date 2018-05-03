1471c508f52d1cbc32cfb3aad739b051a1ca69d8
Tom Hara <tomha@T-HARA.local>
2018-05-03 22:42:39 +0800

    initial

---
+++ wwwroot/js/index.js
@@ -0,0 +1,48 @@
+﻿
+var a = "";
+var s = 123;
+
+//以下使用 extension: faker 插件
+let studentname = "Mr. Stuart Keeling";
+var studentname2 = "Ms. Ludwig Abbott";
+var streetName = "0479 Collier Lane";
+var deptName = "Industrial";
+var deptName2 = "Toys";
+var deptName3 = "Baby"; //where?
+
+
+//alert("hello, Pluralsight!");
+
+console.log("Hello, Pluralsight with Console.log");
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+// var theForm2 = $("theForm");
+// theForm2.hidden = true;
+
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function(){
+    //alert("buying Items.");
+    console.log("buying item.")
+});
+
+var testIMG = document.getElementById("testImg");
+testIMG.addEventListener("mouseover", function(){
+    //alert("hello IMG");
+    console.log("hello img!");
+});
+
+
+//TODO: ? WHY
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
+
+//TODO: fix here?
+//var productInfo2 = document.getElementsByClassName("product-props");
+//var listItems2 = productInfo2.item[0].children;
+
+var hei = document.getElementById("hello");
+
+
